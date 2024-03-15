ee37a8c02ecfb65be285a36a39472acdfcfe9d4b
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2024-03-15 22:47:27 +0100

    Add a4-domainmodel.svg

--- src/core/VIAEventAssociation.Core.Domain/Aggregates/Events/state/IEventStatusState.cs
+++ src/core/VIAEventAssociation.Core.Domain/Aggregates/Events/state/IEventStatusState.cs
@@ -6,22 +6,22 @@ using ViaEventAssociation.Core.Tools.OperationResult;
 namespace VIAEventAssociation.Core.Domain.Aggregates.Events.state;
 
 internal interface IEventStatusState {
-    internal EventStatus CurrentStatus();
-    internal Result UpdateTitle(VeaEvent veaEvent, EventTitle title);
+    EventStatus CurrentStatus();
+    Result UpdateTitle(VeaEvent veaEvent, EventTitle title);
 
-    internal Result UpdateDescription(VeaEvent veaEvent,
+    Result UpdateDescription(VeaEvent veaEvent,
         EventDescription eventDescription);
 
-    internal Result MakePublic(VeaEvent veaEvent);
-    internal Result MakePrivate(VeaEvent veaEvent);
+    Result MakePublic(VeaEvent veaEvent);
+    Result MakePrivate(VeaEvent veaEvent);
 
-    internal Result UpdateMaxNumberOfGuests(VeaEvent veaEvent,
+    Result UpdateMaxNumberOfGuests(VeaEvent veaEvent,
         EventMaxGuests maxGuests);
 
-    internal Result MakeReady(VeaEvent veaEvent);
-    internal Result MakeActive(VeaEvent veaEvent);
-    internal Result MakeCancelled(VeaEvent veaEvent);
-    internal Result UpdateEventDuration(VeaEvent veaEvent, EventDuration eventDuration);
+    Result MakeReady(VeaEvent veaEvent);
+    Result MakeActive(VeaEvent veaEvent);
+    Result MakeCancelled(VeaEvent veaEvent);
+    Result UpdateEventDuration(VeaEvent veaEvent, EventDuration eventDuration);
     Result InviteGuest(VeaEvent veaEvent, EventInvitation invitation);
     Result ParticipateGuest(VeaEvent veaEvent, GuestId guestId);
     Result AcceptInvitation(VeaEvent veaEvent, EventInvitationId invitationId);
