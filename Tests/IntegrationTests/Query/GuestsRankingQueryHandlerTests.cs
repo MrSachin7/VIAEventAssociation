be75bf8707b3eb23ee19b2426e648992a1cdabe7
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2024-04-27 23:34:15 +0200

    Assignment-8 : done and dusted

---
+++ Tests/IntegrationTests/Query/GuestsRankingQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using VIAEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Core.Tools.OperationResult;
+using VIAEventAssociation.Infrastructure.EfcQueries;
+using VIAEventAssociation.Infrastructure.EfcQueries.DataSeeder;
+using VIAEventAssociation.Infrastructure.EfcQueries.QueryHandlers;
+using Xunit.Abstractions;
+
+namespace IntegrationTests.Query;
+
+public class GuestsRankingQueryHandlerTests : ReadContextTestBase {
+
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public GuestsRankingQueryHandlerTests(ITestOutputHelper testOutputHelper) {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public async Task HandleAsync_WhenGuestsExist_ReturnsSuccessResult() {
+        // Arrange
+        VeadatabaseProductionContext context = await SetupReadContext().SeedAllDataAsync();
+        const int guestsPerPage = 5;
+        GuestsRankingQuery.Query query = new GuestsRankingQuery.Query(1, guestsPerPage);
+        GuestsRankingQueryHandler handler = new GuestsRankingQueryHandler(context);
+
+        // Act
+        Result<GuestsRankingQuery.Answer> result = await handler.HandleAsync(query);
+
+
+
+        //Print
+        PrintPayload(result.Payload!, _testOutputHelper);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.NotEmpty(result.Payload!.Guests);
+        Assert.True(result.Payload!.Guests.Count is > 0 and <= guestsPerPage);
+
+
+    }
+}
---
+++ src/Infrastructure/VIAEventAssociation.Infrastructure.EfcQueries/QueryHandlers/GuestsRankingQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Immutable;
+using Microsoft.EntityFrameworkCore;
+using VIAEventAssociation.Core.QueryContracts.Contracts;
+using VIAEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Core.Tools.OperationResult;
+
+namespace VIAEventAssociation.Infrastructure.EfcQueries.QueryHandlers;
+
+public class GuestsRankingQueryHandler: IQueryHandler<GuestsRankingQuery.Query, GuestsRankingQuery.Answer> {
+    private readonly VeadatabaseProductionContext _context;
+
+    public GuestsRankingQueryHandler(VeadatabaseProductionContext context) {
+        _context = context;
+    }
+
+
+    public async Task<Result<GuestsRankingQuery.Answer>> HandleAsync(GuestsRankingQuery.Query query) {
+        IQueryable<Guest> allGuests = _context.Guests.AsQueryable();
+        int totalGuestsAmount = await allGuests.CountAsync();
+        int totalPages = (int) Math.Ceiling((double) totalGuestsAmount / query.GuestsPerPage);
+
+
+
+        ImmutableList<GuestsRankingQuery.Guest> guests = (await allGuests.Include(guest => guest.Events)
+            .OrderByDescending(guest => guest.Events.Count)
+            .Skip(query.GuestsPerPage * (query.PageNumber - 1)).Take(query.GuestsPerPage)
+            .Select(guest => new GuestsRankingQuery.Guest(guest.Id,$"{guest.FirstName} {guest.LastName}", guest.Email, guest.Events.Count))
+            .ToListAsync()).ToImmutableList();
+
+        return new GuestsRankingQuery.Answer(guests, totalPages);
+    }
+}
